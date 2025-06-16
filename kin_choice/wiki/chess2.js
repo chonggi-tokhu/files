1bad067f79aa371c38a318d555bdfbc6ace776b5
colourgrey <120172454+chonggi-tokhu@users.noreply.github.com>
2025-06-16 16:43:25 +0900

    .

--- kin_choice/wiki/chess2.js
+++ kin_choice/wiki/chess2.js
@@ -1698,7 +1698,7 @@ var Chess = function (fen) {
             var ams0 = ms.replace(/(.*?)\{(.*?)\}(.*)/gmi,'$2'); /*ms.replace(/(\{[^}]+\})+?/g, '$1');*/
             console.log(ams0);
             console.log(ms);
-            ms = ms.replace(/(.*?)\{(.*?)\}(.*)/gmi,'$1'+'$2'.replace(/\s+/,'__')+'$3');
+            ms = ms.replace(/(.*?)\{(.*?)\}(.*)/gmi,'$1'+ams0.replace(/\s+/,'__')+'$3');
 
             /* delete recursive annotation variations */
             var rav_regex = /(\([^\(\)]+\))+?/g
