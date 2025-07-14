dba1f82657247605bd8f0d704b86149eb32a3f68
colourgrey <120172454+chonggi-tokhu@users.noreply.github.com>
2025-07-14 15:12:29 +0900

    .

--- kin_choice/new/chess2.js
+++ kin_choice/new/chess2.js
@@ -1700,7 +1700,7 @@ var Chess = function (fen) {
 
             var ams0 = ms.replace(/(.*?)\{(.*?)\}(.*)/gmi, '$2'); /*ms.replace(/(\{[^}]+\})+?/g, '$1');*/
             console.log(ams0);
-            console.log(ms);
+            console.log(ms); 
             ms = ms.replace(ams0,ams0.replaceAll(/\s/gmi,'__'));
             console.log(ms);
             ms = ms.replaceAll(' {','{');
